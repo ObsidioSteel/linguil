7b276552abc0645e24bceaff7b5b0bf25af6c2e2
Charlie McCombie <charliemccombie@gmail.com>
2025-11-20 21:17:45 +0000

    linguil

--- next.config.mjs
+++ next.config.mjs
@@ -97,7 +97,7 @@ const nextConfig = {
           },
           // Mitigates cross-site scripting (XSS) attacks.
           {
-            key: 'Content-Security-Policy',
+            key: 'Content-Security-Policy-Report-Only',
             value: buildCsp(cspPolicies),
           },
           // Controls browser features and APIs.
