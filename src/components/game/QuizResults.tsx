e6e3f4cab18dcef79518b057f673e9e455ab6485
Charlie McCombie <charliemccombie@gmail.com>
2026-03-08 01:26:43 +0000

    linguilv1.3

--- src/components/game/QuizResults.tsx
+++ src/components/game/QuizResults.tsx
@@ -318,7 +318,7 @@ const QuizResults = ({
                     readOnly
                     value={shareText}
                     rows={5}
-                    className="w-auto bg-card p-2 font-sans text-xs dark:text-white rounded-md resize-none ring-2 ring-primary"
+                    className="w-fit h-fit bg-card p-1.5 font-sans text-xs text-center dark:text-white rounded-md resize-none ring-2 ring-primary"
                     // Auto-select all text when the user clicks inside the box.
                     onClick={(e) => (e.target as HTMLTextAreaElement).select()}
                   />
